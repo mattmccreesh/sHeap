e479a42cb73b425fda39c8c81cc50a27efb514a5
Connor Zapfel <czapfel@stevens.edu>
2019-03-27 21:25:49 -0400

    Added stubs for calloc, realloc, and free

--- sheap.c
+++ sheap.c
@@ -1,9 +1,27 @@
 #include <stdlib.h>
 
+/*
+    I think these signatures are right. This is just
+    something to keep in mind. Any alternative signatures
+    may need to be added later on.
+*/
+
+// Allocates the memory and returns a pointer to it
 void* malloc(size_t size){
     return (void*) 0xaabbccdd;
 }
 
-//free
-//calloc
-//realloc
+// Does the same thing as malloc, but zeroes out the memory
+void* calloc(size_t nitems, size_t size){
+    return (void*) 0xaabbccdd;
+}
+
+// Given a pointer to memory and a size, it will attempt to resize the memory chunk
+void* realloc(void* ptr, size_t size){
+    return (void*) 0xaabbccdd;
+}
+
+// Frees a memory allocation pointed to by ptr
+void free(void* ptr){
+    return;
+}
