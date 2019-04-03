a75ad14de687c6efb8015c9ebdc9e90214c6ac62
Thomas Pyle <thomasp162@gmail.com>
2019-04-02 23:21:15 -0400

    Extracts return address and prints it, comparison must be done manually

--- main.c
+++ main.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
+void write_char(char c){
+  write(1, &c, 1);
+}
+
+char hex_digit(int v) {
+  if (v >= 0 && v < 10)
+    return '0' + v;
+  else
+    return 'a' + v - 10; // <-- Here
+}
+
+void print_address_hex(void* p0) {
+  int i;
+  uintptr_t p = (uintptr_t)p0;
+
+  write_char('0'); write_char('x');
+  for(i = (sizeof(p) << 3) - 4; i>=0; i -= 4) {
+    write_char(hex_digit((p >> i) & 0xf));
+  }
+  write_char('\n');
+}
+
 
 int main(){
     void* t = malloc(24);
@@ -8,6 +32,7 @@ int main(){
     char* x = "Worked\0";
     char* xx = "Failed\0";
 
+    print_address_hex(main);
     if(b == 0xaabbccdd){
         write(0,x,8);
     }else{
--- sheap.c
+++ sheap.c
@@ -1,14 +1,39 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
 
 /*
     I think these signatures are right. This is just
     something to keep in mind. Any alternative signatures
     may need to be added later on.
 */
+void write_char(char c){
+  write(1, &c, 1);
+}
+
+char hex_digit(int v) {
+  if (v >= 0 && v < 10)
+    return '0' + v;
+  else
+    return 'a' + v - 10; // <-- Here
+}
+
+void print_address_hex(void* p0) {
+  int i;
+  uintptr_t p = (uintptr_t)p0;
+
+  write_char('0'); write_char('x');
+  for(i = (sizeof(p) << 3) - 4; i>=0; i -= 4) {
+    write_char(hex_digit((p >> i) & 0xf));
+  }
+  write_char('\n');
+}
 
 // Allocates the memory and returns a pointer to it
 void* malloc(size_t size){
-    return (void*) 0xaabbccdd;
+  void* p = __builtin_return_address(0);
+  print_address_hex ( p );
+  return (void*) 0xaabbccdd;
 }
 
 // Does the same thing as malloc, but zeroes out the memory
