06b8769abf31b010f07fbad5abd05ef6f81344e0
Connor Zapfel <czapfel@stevens.edu>
2019-04-10 18:54:10 -0400

    POOL_HASH_TABLE_SIZE is now a #define; Moved POOL_HASH_TABLE up

--- pool_hash_table.h
+++ pool_hash_table.h
@@ -5,6 +5,12 @@
 #include "sheap.h"
 #include "sizetable.h"
 
+// Represents the number of entries in the HT
+#define  POOL_HASH_TABLE_SIZE 250;
+
+// Points to the frist phtEntry in the HT
+struct phtEntry* POOL_HASH_TABLE;
+
 // Struct to represent a HT entry
 struct phtEntry {
     // Stores the call site of the call to malloc
@@ -12,11 +18,6 @@ struct phtEntry {
     // Stores the pointer to the appropriate pool table entry
     struct size_table_elem*     poolPtr;
 };
-
-// Represents the number of entries in the HT
-const int POOL_HASH_TABLE_SIZE = 250;
-// Points to the start of the hash table
-struct phtEntry* POOL_HASH_TABLE = NULL;
 
 // Hashes the call site to an offset into the hash table
 int phtHash(void* callSite);
