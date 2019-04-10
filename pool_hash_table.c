7f9920a64028b114181eb0bdc38f138479ac4260
Connor Zapfel <czapfel@stevens.edu>
2019-04-10 18:54:26 -0400

    Commented a lot of stuff out

--- pool_hash_table.c
+++ pool_hash_table.c
@@ -2,20 +2,24 @@
 #include "sheap.h"
 #include "pool_hash_table.h"
 
-// Load in the pool size constant
-extern const int POOL_HASH_TABLE_SIZE;
 // Load in the pool ht pointer
-extern struct phtEntry* POOL_HASH_TABLE;
+// TODO: vvv This won't compile vvv
+struct phtEntry* POOL_HASH_TABLE = POOL_HASH_TABLE_BASE;
 
-// Initializes the HT with empty entries
+// Load in the pool ht pointer
+struct phtEntry* POOL_HASH_TABLE;
+
+// Generates a hash value for the callSite
 int phtHash(void* callSite){
     return 0;    
 }
 
 // Searches for a hash table entry
 struct phtEntry* phtSearch(void* callSite, size_t allocSize){
+    /*
     // Get the supposed offset into the hash table
     int offset = phtHash(callSite) * sizeof(struct phtEntry);
+    
     // Check to see if a phtEntry exists here
     if(POOL_HASH_TABLE+offset == 0x00000000){
         // If not, lets create one
@@ -23,6 +27,8 @@ struct phtEntry* phtSearch(void* callSite, size_t allocSize){
         (POOL_HASH_TABLE+offset)->poolPtr = create_sizetable_elem(allocSize);
     }
 
-    // Return it
+    // Return the phtEntry ptr
     return (struct phtEntry*) POOL_HASH_TABLE+offset;
+    */
+    return NULL;
 }
