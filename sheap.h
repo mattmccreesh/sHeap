3fe950b7ffeff0c8d106a29daaedfa820c0f7a78
mattmccreesh <mattmccreesh@gmail.com>
2019-04-10 15:39:26 -0400

    updated sheap.h to include global constants

--- sheap.h
+++ sheap.h
@@ -2,5 +2,9 @@
 #define _SHEAP_H
 
 #define BLOCK_SIZE 16384
+void* METAHEAP_BASE; // First value of our metaheap
+void* METAHEAP_END; // First non-valid value
+void* NEXT_FREE_METAHEAP_CHUNK; // Next allocated but unused metaheap location
+void* POOL_HASHTABLE_BASE; // Points to start of the pool hashtable
 
 #endif
