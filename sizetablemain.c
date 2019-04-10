89c742ac5895d95621d1d14bc5b76d18e025a406
mattmccreesh <mattmccreesh@gmail.com>
2019-04-10 12:20:58 -0400

    testing growing the table

--- sizetablemain.c
+++ sizetablemain.c
@@ -3,8 +3,14 @@
 #include <stdlib.h>
 
 extern void* __SHEAP_SIZETABLE_START;
+extern void* __SHEAP_SIZETABLE_END;
 int main()
 {
     initialize_sizetable(20);
+    printf("%d\n", sizeof(struct size_table_elem));
     printf("%p\n", __SHEAP_SIZETABLE_START);
+    printf("%p\n", __SHEAP_SIZETABLE_END);
+    expand_sizetable(10);
+    printf("%p\n", __SHEAP_SIZETABLE_START);
+    printf("%p\n", __SHEAP_SIZETABLE_END);
 }
