eb2e95238b364e406b1bf235c63a986bf77834da
Connor Zapfel <czapfel@stevens.edu>
2019-04-10 18:33:10 -0400

    Globals are now correctly defined

--- sheap.h
+++ sheap.h
@@ -1,16 +1,13 @@
 #ifndef _SHEAP_H
 #define _SHEAP_H
+#include <string.h>
 
 #define BLOCK_SIZE 16384
-void* METAHEAP_BASE; // First value of our metaheap
-void* METAHEAP_END; // First non-valid value
-void* NEXT_FREE_METAHEAP_CHUNK; // Next allocated but unused metaheap location
-void* POOL_HASHTABLE_BASE; // Points to start of the pool hashtable
 
 // Global Meta-Heap Pointers
-void* METAHEAP_BASE = NULL; // First value of our metaheap
-void* METAHEAP_END = NULL; // First non-valid value
-void* NEXT_FREE_METAHEAP_CHUNK = NULL; // Next allocated but unused metaheap location
-void* POOL_HASHTABLE_BASE = NULL; // Points to start of the pool hashtable
+extern void* METAHEAP_BASE; // First value of our metaheap
+extern void* METAHEAP_END; // First non-valid value
+extern void* NEXT_FREE_METAHEAP_CHUNK; // Next allocated but unused metaheap location
+extern void* POOL_HASH_TABLE_BASE; // Points to start of the pool hashtable
 
 #endif
