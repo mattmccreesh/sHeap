495e9e5b8ead3affaacde86849d0d047ec0d1d24
Matt McCreesh <mmccrees@stevens.edu>
2019-04-26 19:40:32 -0400

    removed unnecessary functions for unwinding

--- sheap.c
+++ sheap.c
@@ -11,34 +11,8 @@
 // Load & define global ptr
 void* __SHEAP_BASE = NULL;
 
-int count_valid = 0;
-int count_nptr = 0;
 void* last_ret = (void*) 100;
 void* ret_addr_overwritten;
-
-//unwind stuff
-void* fake_call(){
-    return (void*) 9;
-}
-
-//this should actually be an assembly routine
-void detector(){
-    void* v = fake_call();
-    if(v==last_ret){
-       write_char('W');
-       write_char('\n');
-    }
-    else{
-        write_char('N');
-	write_char('W');
-	write_char('\n');
-    }
-    //asm volatile("mov %0, %r15" : : "r"(ret_addr_overwritten));
-    //asm volatile("mov %0, %rax" : : "r"(v));
-    asm("mov %0, %%r11" : : "r"(ret_addr_overwritten));
-    asm("mov %0, %%rax" : : "r"(v));
-    asm("jmp *%r11");
-}
 
 
 //overwrite with this addr + 4 (4 to avoid prologue of function)
@@ -62,29 +36,6 @@ jmpBack:
   asm("mov %0, %%r11" : : "r"(ret_addr_overwritten));
   asm("pop %rax");//restore stack, get rax back to propogate return value properly
   asm("jmp *%r11");
-}
-
-
-void* unwinder()
-{
-    unw_cursor_t cursor;
-    unw_context_t uc;
-    unw_word_t ip;
-    unw_word_t sp;
-    unw_getcontext(&uc);
-    unw_init_local(&cursor, &uc);
-    unw_step(&cursor);
-    unw_step(&cursor);
-    unw_get_reg(&cursor, UNW_REG_IP, &ip);
-    printf("instruction pointer at %lx\n", (long)ip);
-    unw_get_reg(&cursor, UNW_REG_SP, &sp);
-    sp -= 8;//minus 8 bytes for return address
-    printf("return address at %lx\n", (long)sp);
-    last_ret = 0;//value returned by us
-    ret_addr_overwritten = (void*) ip;
-    void** ret_addr_to_overwrite = (void**) sp;
-    *ret_addr_to_overwrite = &detector + 18;
-    return (void*) 0;
 }
 
 
